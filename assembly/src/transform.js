cdeaaea8759e60badf33cfa4bb5015707837b9fc
Lane Rettig <lanerettig@gmail.com>
2018-07-25 16:18:55 -0400

    Basic ABI router method successfully building

--- assembly/src/transform.js
+++ assembly/src/transform.js
@@ -20,6 +20,15 @@ const {
 } = assemblyscript
 const keccak256 = require("js-sha3").keccak256
 
+/**
+ * This function implements a basic, bare-bones ABI as a proof of concept
+ * based on the simplistic ABI laid out here:
+ * https://gist.github.com/axic/16158c5c88fbc7b1d09dfa8c658bc363
+ *
+ * TODO: Transition to full ABI support using
+ https://github.com/ethereumjs/ethereumjs-abi (or web3.js, ethjs-abi,
+ etherjs-abi libraries per @axic)
+ */
 exports.afterParse = function (parser) {
   // Only consider entry source
   const entrySrc = parser.program.sources.find(s => s.isEntry)
@@ -52,6 +61,7 @@ exports.afterParse = function (parser) {
 `)
 
       // Process the methods; skip the constructor
+      let abiFunction = ""
       contractStmt.members.filter(m =>
         m.kind === NodeKind.METHODDECLARATION && !m.is(CommonFlags.CONSTRUCTOR)
       ).forEach(method => {
@@ -61,41 +71,96 @@ exports.afterParse = function (parser) {
         // Loop over the params for these next steps
         let paramsWrapper = ""
         let methodSignature = ""
-        let currentPlace = 0
+        let argList = ""
+        // Start reading from fifth byte (selector, above, comes first)
+        let currentPlace = 4
         let varNum = 0
         method.signature.parameters.forEach(p => {
           methodSignature += methodSignature ? "," : ""
           methodSignature += p.type.name.text
+          argList += argList ? "," : ""
+          // TODO: How to handle Address? What kind and name does it have?
           switch (p.type.kind) {
             case TypeKind.I16:
-              paramsWrapper += (
-                `var ptr${varNum} = <i32>memory.allocate(20)
-                 callDataCopy(ptr${varNum}, ${currentPlace}, 20)
-                `
+              paramsWrapper += (`
+  var ptrParam${varNum} = <i32>memory.allocate(20)
+  callDataCopy(ptr${varNum}, ${currentPlace}, 20)
+`
               )
               currentPlace += 20
+              argList += `ptr${varNum}`
               break
             default:
               throw new Error("Unsupported type found in params")
           }
+          varNum++
         })
-        const returnSignature = method.signature.returnType ? `(${method.signature.returnType.name.text})` : ""
-        const signature = `${method.name.text}(${methodSignature})${returnSignature}`
 
-        let abiFunction = (
-          `function ${method.name.text}_wrapper() {
-            var dataSize = getCallDataSize()
-            if (datasize !== ${currentPlace})
-              throw new Error("Bad call data length")
+        abiFunction += (
           `
+function ${method.name.text}_wrapper(): void {
+  var datasize:u32 = getCallDataSize()
+  if (datasize !== ${currentPlace})
+    throw new Error("Bad call data length")
+   ${paramsWrapper}
+`
         )
+        // Handle method return vals
+        let returnsWrapper = ''
+        let returnType
+        let returnSignature = ''
+        // TODO: for some reason kind !== TypeKind.VOID
+        if (method.signature.returnType.name.text === 'void') {
+          abiFunction += (
+            `  // Call method with loaded args, no return value
+  contract.${method.name.text}(${argList})
+  return(0, 0)
+}
+`
+          )
+        } else {
+          returnSignature = `(${method.signature.returnType.name.text})`
+          switch (method.signature.returnType.kind) {
+            case TypeKind.I16:
+              returnType = 'i16'
+              returnsWrapper = (
+                `
+  // Store return value
+  var ptrReturn = <i32>memory.allocate(32)
+  store<i32>(ptrReturn, retval)
 
+  // Call return
+  return(ptrReturn, 32)
+}
+`
+              )
+              break
+            default:
+              throw new Error("Unsupported return type: " + method.signature.returnType.name.text)
+          }
+          abiFunction += (
+            `  // Call method with loaded args, capture return value
+  var retval:${returnType} = contract.${method.name.text}(${argList})
+`
+          ) + returnsWrapper
+        }
+
+        const signature = `${method.name.text}(${methodSignature})${returnSignature}`
         const abssig = keccak256(signature).substring(0,8)
         console.log("Generated signature for method:", signature, ", abssig:", abssig)
-        abiRouter += `case 0x${abssig}: contract.${method.name.text}(); break; `
+        abiRouter += (
+          `    case 0x${abssig}: ${method.name.text}_wrapper(); break
+`
+        )
       });
 
-      abiRouter += 'default: revert(0, 0)}}'
+      abiRouter += (
+        `    default: revert(0, 0)
+  }
+}
+`
+      )
+      abiRouter = abiFunction + abiRouter
       console.log("abiRouter:", abiRouter)
 
       // Find the right source
@@ -114,7 +179,6 @@ exports.afterParse = function (parser) {
       }
       else
         throw new Error("Found no main source")
-      true
     }
   }
 }
